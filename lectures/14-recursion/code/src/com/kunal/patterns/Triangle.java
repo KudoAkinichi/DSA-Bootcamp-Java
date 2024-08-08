94a49d6a3872816de575d7e17170712af1322e82
Aryan Krishna <105040194+KudoAkinichi@users.noreply.github.com>
2024-08-09 02:09:11 +0530

    recursion

--- lectures/14-recursion/code/src/com/kunal/patterns/Triangle.java
+++ lectures/14-recursion/code/src/com/kunal/patterns/Triangle.java
@@ -4,9 +4,10 @@ import java.util.Arrays;
 
 public class Triangle {
     public static void main(String[] args) {
-//        triangle2(4, 0);
-        int[] arr = {1, 4, 3, 5};
+        // triangle2(4, 0);
+        int[] arr = { 1, 4, 3, 5 };
         selection(arr, arr.length, 0, 0);
+        // bubble(arr, arr.length - 1, 0);
         System.out.println(Arrays.toString(arr));
     }
 
@@ -15,10 +16,10 @@ public class Triangle {
             return;
         }
         if (c < r) {
-            triangle2(r, c+1);
+            triangle2(r, c + 1);
             System.out.print("*");
         } else {
-            triangle2(r-1, 0);
+            triangle2(r - 1, 0);
             System.out.println();
         }
     }
@@ -29,13 +30,12 @@ public class Triangle {
         }
         if (c < r) {
             System.out.print("*");
-            triangle(r, c+1);
+            triangle(r, c + 1);
         } else {
             System.out.println();
-            triangle(r-1, 0);
+            triangle(r - 1, 0);
         }
     }
-
 
     static void bubble(int[] arr, int r, int c) {
         if (r == 0) {
@@ -43,16 +43,16 @@ public class Triangle {
         }
         if (c < r) {
 
-            if (arr[c] > arr[c+1]) {
+            if (arr[c] > arr[c + 1]) {
                 // swap
                 int temp = arr[c];
-                arr[c] = arr[c+1];
-                arr[c+1] = temp;
+                arr[c] = arr[c + 1];
+                arr[c + 1] = temp;
             }
 
-            bubble(arr, r, c+1);
+            bubble(arr, r, c + 1);
         } else {
-            bubble(arr, r-1, 0);
+            bubble(arr, r - 1, 0);
         }
     }
 
@@ -62,17 +62,16 @@ public class Triangle {
         }
         if (c < r) {
             if (arr[c] > arr[max]) {
-                selection(arr, r, c+1, c);
+                selection(arr, r, c + 1, c);
             } else {
-                selection(arr, r, c+1, max);
+                selection(arr, r, c + 1, max);
             }
         } else {
             int temp = arr[max];
-            arr[max] = arr[r-1];
-            arr[r-1] = temp;
-            selection(arr, r-1, 0, 0);
+            arr[max] = arr[r - 1];
+            arr[r - 1] = temp;
+            selection(arr, r - 1, 0, 0);
         }
     }
-
 
 }
